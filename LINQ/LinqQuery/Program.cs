6e2420c318c9e82dca965d2554e985d135e36cb0
lcsmota <sacul_m@protonmail.com>
2023-03-09 10:49:09 -0300

    Feat: query operator OfType

--- LINQ/LinqQuery/Program.cs
+++ LINQ/LinqQuery/Program.cs
@@ -1,4 +1,4 @@
-﻿// #region LINQ Query Syntax and Query Method
+﻿#region LINQ Query Syntax and Query Method
 // IList<string> languages = new List<string>
 // {
 //     "C#",
@@ -25,56 +25,95 @@
 // foreach (var rst2 in result2)
 //     Console.WriteLine(rst2);
 
-// #endregion
+#endregion
 
 #region Lambda Expression, Func, Action, Query Operator: Where 
-IList<Student> students = new List<Student>()
+// IList<Student> students = new List<Student>()
+// {
+//     new Student() {Id = 1, Name = "Bill", Age = 25},
+//     new Student() {Id = 2, Name = "John", Age = 35},
+//     new Student() {Id = 3, Name = "Mary", Age = 22},
+//     new Student() {Id = 4, Name = "Ricardo", Age = 32},
+//     new Student() {Id = 5, Name = "Sophie", Age = 17},
+//     new Student() {Id = 6, Name = "Josh", Age = 55},
+//     new Student() {Id = 7, Name = "Aaron", Age = 67},
+//     new Student() {Id = 7, Name = "Julia", Age = 14},
+//     new Student() {Id = 7, Name = "Gustavo", Age = 19},
+//     new Student() {Id = 7, Name = "Mirian", Age = 11}
+// };
+
+// Func<Student, bool> isStudentTeenAger = e => e.Age > 12 && e.Age < 20;
+// Action<Student> PrintStudentDetail = e => Console.WriteLine($"\t{e.Name} - {e.Age}");
+
+// var filteredAdultStudents = from adult in students
+//                             where adult.Age > 18
+//                             select adult;
+
+// var filteredTeenAgerStudents = students.Where(isStudentTeenAger).ToList<Student>();
+
+// var filteredStudentsWithIndex = students.Where((std, index) =>
+// {
+//     if (index % 2 == 0) return true;
+
+//     return false;
+// });
+
+// Console.WriteLine();
+// foreach (var std in filteredStudentsWithIndex)
+//     PrintStudentDetail(std);
+
+// Console.WriteLine("Teen: ");
+// foreach (var teen in filteredTeenAgerStudents)
+//     PrintStudentDetail(teen);
+
+// Console.WriteLine("Adult: ");
+// foreach (var adult in filteredAdultStudents)
+//     PrintStudentDetail(adult);
+
+// class Student
+// {
+//     public int Id { get; set; }
+//     public string Name { get; set; }
+//     public int Age { get; set; }
+// }
+#endregion
+
+#region Query Operator: OfType
+IList<object> mixedList = new List<object>();
+mixedList.Add(0);
+mixedList.Add("One");
+mixedList.Add("Juca Monteiro");
+mixedList.Add(3);
+mixedList.Add(new Person { Name = "Karlos", Age = 25 });
+
+var stringResult = from result in mixedList.OfType<string>()
+                   select result;
+
+var intResult = from result in mixedList.OfType<int>()
+                select result;
+
+var objectResult = mixedList.OfType<Person>();
+
+Console.WriteLine("String type:");
+foreach (var str in stringResult)
+    Console.WriteLine(str);
+
+Console.WriteLine("Int type:");
+foreach (var integer in intResult)
+    Console.WriteLine(integer);
+
+Console.WriteLine("Object type:");
+foreach (var obj in objectResult)
+    Console.WriteLine(obj.ToString());
+
+class Person
 {
-    new Student() {Id = 1, Name = "Bill", Age = 25},
-    new Student() {Id = 2, Name = "John", Age = 35},
-    new Student() {Id = 3, Name = "Mary", Age = 22},
-    new Student() {Id = 4, Name = "Ricardo", Age = 32},
-    new Student() {Id = 5, Name = "Sophie", Age = 17},
-    new Student() {Id = 6, Name = "Josh", Age = 55},
-    new Student() {Id = 7, Name = "Aaron", Age = 67},
-    new Student() {Id = 7, Name = "Julia", Age = 14},
-    new Student() {Id = 7, Name = "Gustavo", Age = 19},
-    new Student() {Id = 7, Name = "Mirian", Age = 11}
-};
-
-Func<Student, bool> isStudentTeenAger = e => e.Age > 12 && e.Age < 20;
-Action<Student> PrintStudentDetail = e => Console.WriteLine($"\t{e.Name} - {e.Age}");
-
-var filteredAdultStudents = from adult in students
-                            where adult.Age > 18
-                            select adult;
-
-var filteredTeenAgerStudents = students.Where(isStudentTeenAger).ToList<Student>();
-
-var filteredStudentsWithIndex = students.Where((std, index) =>
-{
-    if (index % 2 == 0) return true;
-
-    return false;
-});
-
-Console.WriteLine();
-foreach (var std in filteredStudentsWithIndex)
-    PrintStudentDetail(std);
-
-Console.WriteLine("Teen: ");
-foreach (var teen in filteredTeenAgerStudents)
-    PrintStudentDetail(teen);
-
-Console.WriteLine("Adult: ");
-foreach (var adult in filteredAdultStudents)
-    PrintStudentDetail(adult);
-
-class Student
-{
-    public int Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
+
+    public override string ToString()
+        => $"{Name} - {Age}";
 }
 #endregion
+
 
