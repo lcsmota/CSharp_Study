0b4dd304962121256b0f53aa5416f039b5bd53d6
lcsmota <sacul_m@protonmail.com>
2023-03-09 10:12:21 -0300

    Feat: LINQ query syntax and query method

---
+++ LINQ/LinqQuery/Program.cs
@@ -0,0 +1,25 @@
+﻿IList<string> languages = new List<string>
+{
+    "C#",
+    "Java",
+    "C++",
+    "Ruby",
+    "Rust",
+    "JavaScript",
+    "PHP",
+    "Python",
+    "Elixir",
+    "Lua",
+    "Haskell"
+};
+
+var result = from lang in languages
+             where lang.Contains("u")
+             select lang;
+
+foreach (var rst in result)
+    Console.WriteLine(rst);
+
+var result2 = languages.Where(e => e.Contains("a"));
+foreach (var rst2 in result2)
+    Console.WriteLine(rst2);
